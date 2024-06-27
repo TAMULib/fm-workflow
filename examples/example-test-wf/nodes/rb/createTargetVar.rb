03d392ca9c061a7196909a78f30639a55d7e4cc9
Rajdeep Hundal <rhundal7@tamu.edu>
2024-06-27 13:25:50 -0500

    workflow in progress

--- examples/example-test-wf/nodes/rb/createTargetVar.rb
+++ examples/example-test-wf/nodes/rb/createTargetVar.rb
@@ -1,3 +1,12 @@
 
+require 'json'
+
+query_wrapper = {
+  message: 'Hello, File From the Script Task!'
+}
+
+json_string  = query_wrapper.to_json
+execution.setVariableLocal('exampleFileData', json_string)
+
 execution.setVariable('exampleFileData', 'Hello, File From Script Task!')
 
