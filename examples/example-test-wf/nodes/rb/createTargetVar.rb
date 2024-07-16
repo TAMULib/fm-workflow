9cbfc5177f5039dfdc94389bbacd3186e1482d47
Rajdeep Hundal <rhundal7@tamu.edu>
2024-07-16 16:09:39 -0500

    wip

--- examples/example-test-wf/nodes/rb/createTargetVar.rb
+++ examples/example-test-wf/nodes/rb/createTargetVar.rb
@@ -2,11 +2,11 @@
 require 'json'
 
 query_wrapper = {
-  message: 'Hello, File From the Script Task!'
+  message: 'Hello, File From Script Task. You need me to create File Task!'
 }
 
 json_string  = query_wrapper.to_json
 execution.setVariableLocal('exampleFileData', json_string)
 
-execution.setVariable('exampleFileData', 'Hello, File From Script Task!')
+execution.setVariable('exampleFileData', 'Hello, File From Script Task. You need me to create File Task!')
 
